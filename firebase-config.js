76d67687f7b43424f1f9e5490705b2ae37555c60
duong <duongnanya@gmail.com>
2025-09-14 22:54:36 +0700

    Update .gitignore to allow firebase-config.js template file

---
+++ firebase-config.js
@@ -0,0 +1,13 @@
+// Firebase Configuration - Placeholder values
+// Real values will be injected by GitHub Actions during deployment
+const firebaseConfig = {
+    apiKey: "PLACEHOLDER_API_KEY",
+    authDomain: "PLACEHOLDER_AUTH_DOMAIN",
+    projectId: "PLACEHOLDER_PROJECT_ID",
+    storageBucket: "PLACEHOLDER_STORAGE_BUCKET",
+    messagingSenderId: "PLACEHOLDER_MESSAGING_SENDER_ID",
+    appId: "PLACEHOLDER_APP_ID"
+};
+
+// Export config để sử dụng trong các file khác
+window.firebaseConfig = firebaseConfig;
